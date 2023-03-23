d4aa3df27e8933baa16882a3ef3ec98661bcac38
Emir Arı <emirariomatic@gmail.com>
2023-03-23 23:43:26 +0300

    initial commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Widget buildTextField() {
+    return Container(
+      color: const Color.fromARGB(255, 204, 215, 236),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          children: [
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: 'Enter your text here'),
+              ),
+            ),
+            IconButton(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              onPressed: () {},
+              icon: const Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 222, 235, 207),
+        title: Text('OpenAI Test'),
+      ),
+      body: Container(
+        child: buildTextField(),
+      ),
+    );
+  }
+}
